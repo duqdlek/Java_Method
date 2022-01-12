25ecc4d68a20b0175466c2a4383d31997f893692
sangyeop oh <yeo1125@naver.com>
2022-01-12 16:47:58 +0900

    for study

---
+++ FirstMethod.java
@@ -0,0 +1,11 @@
+
+public class FirstMethod {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println(Math.floor(1.1));
+	
+	}
+
+}
---
+++ WhyMethod.java
@@ -0,0 +1,18 @@
+
+public class WhyMethod {
+
+	public static void main(String[] args) {
+		                      //인자,argument
+		printTwoTimes("a","#");
+		printTwoTimes("a","$");
+		printTwoTimes("a","!");
+		printTwoTimes("b","-");
+	}
+                                                       //매개변수,parameter	
+	public static void printTwoTimes(String text, String delimiter) {
+		System.out.println(delimiter);
+		System.out.println(text);
+		System.out.println(text);
+	}
+	
+}
